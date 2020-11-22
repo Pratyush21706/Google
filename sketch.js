1c62b4567dc30b3e94896abf01efb78a9a73b760
Pratyush21706 <65002484+Pratyush21706@users.noreply.github.com>
2020-11-22 23:07:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup(){
+}
+
+function draw(){
+    if(window.innerWidth>799){
+        console.log('I will do It ')
+            select(`.logo`).style(`font-size`,`70pt`)
+//                              select(`.search`).style(`margin-bottom`,`-200px`)
+                              select(`.hr`).style(`margin-left`,`89%`)
+
+
+
+    }
+      if(window.innerWidth<799){
+        console.log('I will do It ')
+            select(`.logo`).style(`font-size`,`50pt`)
+                              select(`.hr`).style(`margin-left`,`68%`)
+
+//margin-top
+    }
+}
